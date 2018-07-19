94d5d325ea2215826cf6e71572701b112f789c8c
Anwesh43 <anweshthecool0@gmail.com>
2018-07-19 10:04:44 +0530

    adding canvas extension methods

--- bmsview/src/main/java/com/anwesh/uiprojects/bmsview/BMSView.kt
+++ bmsview/src/main/java/com/anwesh/uiprojects/bmsview/BMSView.kt
@@ -9,8 +9,41 @@ import android.graphics.Canvas
 import android.view.View
 import android.view.MotionEvent
 import android.content.Context
+import android.graphics.Color
 
 val BMS_NODES : Int = 5
+
+fun Canvas.drawBall(gap : Float, y : Float, r : Float, scale : Float,  paint : Paint) {
+    paint.style = Paint.Style.STROKE
+    save()
+    translate(gap * scale, y - r)
+    rotate(360f * scale)
+    drawCircle(0f, 0f, r, paint)
+    drawLine(0f, 0f, 0f, -r * 0.6f, paint)
+    restore()
+}
+
+fun Canvas.drawStep(gap : Float, scale : Float, paint : Paint) {
+    drawLine(0f, 0f, gap, -gap * scale, paint)
+}
+
+fun Canvas.drawBSMNode(i : Int, j : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = (Math.min(w, h) * 0.9f) / BMS_NODES
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap  = Paint.Cap.ROUND
+    paint.color = Color.parseColor("#2ecc71")
+    save()
+    translate(0.05f * w + i * gap, 0.95f * h - i * gap)
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f)) * 2
+    drawStep(gap, sc1, paint)
+    if (i == j) {
+        drawBall(gap, -gap * sc2, gap/10, sc2, paint)
+    }
+    restore()
+}
 
 class BMSView(ctx : Context) : View(ctx) {
 
