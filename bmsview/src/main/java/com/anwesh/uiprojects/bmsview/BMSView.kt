e18194be18edcb6f0cd2e5fee5dc4e4ba65a232d
Anwesh43 <anweshthecool0@gmail.com>
2018-07-19 09:40:43 +0530

    adding BMSView class

--- bmsview/src/main/java/com/anwesh/uiprojects/bmsview/BMSView.kt
+++ bmsview/src/main/java/com/anwesh/uiprojects/bmsview/BMSView.kt
@@ -11,3 +11,21 @@ import android.view.MotionEvent
 import android.content.Context
 
 val BMS_NODES : Int = 5
+
+class BMSView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
