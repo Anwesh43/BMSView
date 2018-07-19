c474c623eb1c444966fd34563cc24579537e5aac
Anwesh43 <anweshthecool0@gmail.com>
2018-07-19 11:13:59 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/ballmoverstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/ballmoverstepview/MainActivity.kt
@@ -2,10 +2,13 @@ package com.anwesh.uiprojects.ballmoverstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bmsview.BMSView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        BMSView.create(this)
+
     }
 }
