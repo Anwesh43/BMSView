61de7e48f31e832d64e0d29793ca3130f99c4f8f
Anwesh43 <anweshthecool0@gmail.com>
2018-07-19 09:26:22 +0530

    importing modules`

--- bmsview/src/main/java/com/anwesh/uiprojects/bmsview/BMSView.kt
+++ bmsview/src/main/java/com/anwesh/uiprojects/bmsview/BMSView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.bmsview
 /**
  * Created by anweshmishra on 19/07/18.
  */
+
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+
+val BMS_NODES : Int = 5
